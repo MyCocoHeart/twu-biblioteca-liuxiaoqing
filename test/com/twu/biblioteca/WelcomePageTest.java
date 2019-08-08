3f0c049b66bc6edb63b40f4974e3b668e33ec08b
xq <xiaoq_xiaoq@163.com>
2019-08-08 16:32:18 +0800

    refactoried the WelcomePageTest Class

--- test/com/twu/biblioteca/WelcomePageTest.java
+++ test/com/twu/biblioteca/WelcomePageTest.java
@@ -32,8 +32,8 @@ public class WelcomePageTest {
 
     @Test
     public void shouldPrintWelcomeMassages() {
-        String massage= "Welocme to Biblioteca. You one-stop-shop for great book titles in Bangalore!";
-        WelcomePage welcomePage = new WelcomePage(massage);
+        String message= "Welocme to Biblioteca. You one-stop-shop for great book titles in Bangalore!";
+        WelcomePage welcomePage = new WelcomePage(message);
         welcomePage.showWelcomeMessages();
         String expected = new String("Welocme to Biblioteca. You one-stop-shop for great book titles in Bangalore!");
         assertEquals(expected,bytes.toString().trim().replace("\r",""));
