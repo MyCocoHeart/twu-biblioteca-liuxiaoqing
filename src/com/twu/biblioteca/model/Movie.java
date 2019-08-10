d06cab07c3d8e529f43e2e99301955f141c52879
xq <xiaoq_xiaoq@163.com>
2019-08-10 13:55:28 +0800

    add a movie object

--- src/com/twu/biblioteca/model/Movie.java
+++ src/com/twu/biblioteca/model/Movie.java
@@ -1,8 +1,72 @@
 package com.twu.biblioteca.model;
 
+import sun.management.ThreadInfoCompositeData;
+
 /**
  * @author:xiaoq
  * @date:2019/8/10 13:35
  */
 public class Movie {
+    private Integer id;
+    private String name;
+    private String year;
+    private String director;
+    private String rating;
+    private boolean isCheckouted;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public boolean isCheckouted() {
+        return isCheckouted;
+    }
+
+    public void setCheckouted(boolean checkouted) {
+        isCheckouted = checkouted;
+    }
+
+    public Movie(Integer id,String name,String year,String director,String rating) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+        this.isCheckouted = false;
+    }
+
+
+
 }
---
+++ test/com/twu/biblioteca/MovieTest.java
@@ -0,0 +1,18 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.model.Movie;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author:xiaoq
+ * @date:2019/8/10 13:38
+ */
+public class MovieTest {
+    @Test
+    public void creatAMovieObject() {
+        Movie movie = new Movie(1,"疯狂动物城","2016","里奇·摩尔","9.2");
+    }
+
+}
